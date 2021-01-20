2465a20a58a13fe51ffd618b4f5b5dd949486d26
Marcin <mbielen1995@gmail.com>
2021-01-20 18:03:45 +0100

    Switch

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,24 +16,24 @@ public class JavaApplication1 {
         //byte, short, long
         //char
         final double NUMBER_PI = 3.14; //const final unchangeable variable
-        
+
         c = a + b;
-        
+
         System.out.println("Cześć " + firstName + "!");
         System.out.println("Liczba: " + c);
         System.out.println("Liczba pi: " + NUMBER_PI);
-        
+
         //casting
         System.out.println(b / a);
         System.out.println(b / (double)a);
-        
+
         int result1 = b / (int)f;
         double result2 = b / f;
-        
+
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(1.0 / 5);
-        
+
         /*
         0 - false 1 - true
         ~ - not
@@ -43,17 +43,30 @@ public class JavaApplication1 {
         x << 1 - 1 bit left shift
         x >> 2 - 2 bit right shift
         */
-         
-       if (a > b)
+
+        if (a > b)
             System.out.println("a > b");
-       else if (a < b)
-       {
+        else if (a < b)
+        {
             System.out.println("a < b");  
-       }
-       else
-       {
+        }
+        else
+        {
             System.out.println("a == b");
-       }
+        }
+       
+        int num = 3; 
+        switch (num)
+        {
+            case 1:
+                System.out.println("Pierwszy przypadek");
+                break;
+            case 2:
+                System.out.println("Drugi przypadek");
+                break;
+            default:
+                System.out.println("Inny przypadek");
+        }
     }
     
 }
